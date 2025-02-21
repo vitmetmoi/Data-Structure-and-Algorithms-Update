6a0051282ddd717beca3c5312333475fc4dc6f9d
Bảo Duy <132726465+vitmetmoi@users.noreply.github.com>
2025-02-21 07:47:15 +0700

    update 1 file and create 2 files

---
+++ final/Addition.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    int sum;
+    scanf("%d", &sum);
+    int a, b;
+    a = sum - 0;
+    b = sum - a;
+    printf("%d %d", a, b);
+}
